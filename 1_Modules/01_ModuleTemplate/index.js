724a0b08661292bd4ffe6a8e77f44e5c246bc358
Federico B Dela Pena <rico.delapena.banzon@gmail.com>
2022-12-15 13:20:03 +0800

    deleted readme.txt

---
+++ 1_Modules/01_ModuleTemplate/index.js
@@ -0,0 +1,5 @@
+/* import the exported module from path */
+import sampleModule from './sampleModule'
+
+/* sample call of the module */
+sampleModule();
---
+++ 1_Modules/01_ModuleTemplate/sampleModule.js
@@ -0,0 +1,8 @@
+/* Sample module */
+function sampleModule(){
+    console.log("I am a sample module.");
+};
+
+/* Export */
+/* export default sampleModule
+ */
